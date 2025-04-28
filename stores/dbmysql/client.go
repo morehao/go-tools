21a88e5f6b7795798049a2a28e81e8fe4fd63067
morehao <morehao@qq.com>
2025-04-28 22:06:17 +0800

    feat(dbmysql): 支持多 client 初始化

--- stores/dbmysql/client.go
+++ stores/dbmysql/client.go
@@ -1,16 +1,17 @@
 package dbmysql
 
 import (
-	"context"
-	"errors"
 	"fmt"
+	"sync"
 	"time"
 
-	"github.com/morehao/go-tools/glog"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-	"gorm.io/gorm/utils"
+)
+
+var (
+	dbMap = map[string]*gorm.DB{}
+	lock  sync.Mutex
 )
 
 type MysqlConfig struct {
@@ -28,6 +29,9 @@ type MysqlConfig struct {
 }
 
 func InitMysql(cfg MysqlConfig) (*gorm.DB, error) {
+	if cfg.Service == "" {
+		return nil, fmt.Errorf("service name is empty")
+	}
 	dns := cfg.buildDns()
 	customLogger, newLogErr := newOrmLogger(&ormConfig{
 		Service:   cfg.Service,
@@ -44,6 +48,9 @@ func InitMysql(cfg MysqlConfig) (*gorm.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+	lock.Lock()
+	defer lock.Unlock()
+	dbMap[cfg.Service] = db
 	return db, nil
 }
 
@@ -59,102 +66,18 @@ func (cfg *MysqlConfig) buildDns() string {
 	return dns
 }
 
-type ormLogger struct {
-	Service       string
-	Addr          string
-	Database      string
-	MaxSqlLen     int
-	SlowThreshold time.Duration
-	Logger        glog.Logger
-}
-
-type ormConfig struct {
-	Service   string
-	Addr      string
-	Database  string
-	MaxSqlLen int
-}
-
-func newOrmLogger(cfg *ormConfig) (*ormLogger, error) {
-	s := cfg.Service
-	if cfg.Service == "" {
-		s = cfg.Database
+func InitMultiMysql(cfgs []MysqlConfig) error {
+	for _, cfg := range cfgs {
+		_, err := InitMysql(cfg)
+		if err != nil {
+			return err
+		}
 	}
-	l, err := glog.GetModuleLogger("gorm", glog.WithCallerSkip(5))
-	if err != nil {
-		return nil, err
-	}
-	return &ormLogger{
-		Service:   s,
-		Addr:      cfg.Addr,
-		Database:  cfg.Database,
-		MaxSqlLen: cfg.MaxSqlLen,
-		Logger:    l,
-	}, nil
+	return nil
 }
 
-// LogMode log mode
-func (l *ormLogger) LogMode(level logger.LogLevel) logger.Interface {
-	return l
-}
-
-// Info print info
-func (l *ormLogger) Info(ctx context.Context, msg string, data ...interface{}) {
-	formatMsg := fmt.Sprintf(msg, append([]interface{}{utils.FileWithLineNum()}, data...)...)
-	l.Logger.Infow(ctx, formatMsg, l.commonFields(ctx)...)
-}
-
-// Warn print warn messages
-func (l *ormLogger) Warn(ctx context.Context, msg string, data ...interface{}) {
-	formatMsg := fmt.Sprintf(msg, append([]interface{}{utils.FileWithLineNum()}, data...)...)
-	l.Logger.Warnw(ctx, formatMsg, l.commonFields(ctx)...)
-}
-
-// Error print error messages
-func (l *ormLogger) Error(ctx context.Context, msg string, data ...interface{}) {
-	formatMsg := fmt.Sprintf(msg, append([]interface{}{utils.FileWithLineNum()}, data...)...)
-	l.Logger.Errorw(ctx, formatMsg, l.commonFields(ctx)...)
-}
-
-// Trace print sql message
-func (l *ormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
-	end := time.Now()
-	cost := glog.GetRequestCost(begin, end)
-
-	msg := "sql execute success"
-	var ralCode int
-	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
-		// 过滤未找到数据的错误
-		msg = err.Error()
-		ralCode = -1
-	}
-	sql, rows := fc()
-	if len(sql) > l.MaxSqlLen && l.MaxSqlLen > 0 {
-		sql = sql[:l.MaxSqlLen]
-	}
-
-	// fileLineNum := utils.FileWithLineNum()
-	fields := l.commonFields(ctx)
-	fields = append(fields,
-		glog.KeyAffectedRows, rows,
-		// glog.KeyFile, fileLineNum,
-		glog.KeyCost, cost,
-		glog.KeyRalCode, ralCode,
-		glog.KeySql, sql,
-	)
-
-	if l.SlowThreshold > 0 && cost >= float64(l.SlowThreshold/time.Millisecond) {
-		msg = "slow sql"
-		l.Logger.Warnw(ctx, msg, fields...)
-	} else {
-		l.Logger.Debugw(ctx, msg, fields...)
-	}
-}
-
-func (l *ormLogger) commonFields(ctx context.Context) []interface{} {
-	fields := []interface{}{
-		glog.KeyAddr, l.Addr,
-		glog.KeyDatabase, l.Database,
-	}
-	return fields
+func GetDB(service string) *gorm.DB {
+	lock.Lock()
+	defer lock.Unlock()
+	return dbMap[service]
 }
