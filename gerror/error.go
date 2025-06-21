1969ac9387fb4b47b44d669af92c44ef07b966e6
morehao <morehao@qq.com>
2025-06-22 01:24:18 +0800

    feat(gerror): remove pkg/errors mod (#39)
    * feat(codegen): 代码生成返回数据库默认值相关信息
    
    * feat(gerror): remove pkg/errors mod

--- gerror/error.go
+++ gerror/error.go
@@ -1,9 +1,8 @@
 package gerror
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/pkg/errors"
 )
 
 type Error struct {
@@ -14,40 +13,40 @@ type Error struct {
 type ErrorMap map[int]Error
 type CodeMsgMap map[int]string
 
-// Error 方法实现了error接口，返回错误信息
+// Error 方法实现 error 接口
 func (e Error) Error() string {
 	return e.Msg
 }
 
-// Wrap 用于包装错误信息
-func (e Error) Wrap(err error) error {
+// Wrap 用于包装错误信息（使用标准库 fmt.Errorf）
+func (e *Error) Wrap(err error) error {
 	if err == nil {
 		return nil
 	}
 
-	// 保存原始错误信息
 	msg := e.Msg
-	// 更新错误信息，方便后续使用
-	e.Msg = err.Error()
-	return errors.Wrap(err, msg)
+	e.Msg = err.Error() // 可选：记录原始错误内容
+	return fmt.Errorf("%s: %w", msg, err)
 }
 
-func (e Error) Wrapf(err error, format string, args ...interface{}) error {
+// Wrapf 用于格式化包装错误信息
+func (e *Error) Wrapf(err error, format string, args ...interface{}) error {
 	if err == nil {
 		return nil
 	}
 
-	// 保存原始错误信息
 	msg := e.Msg
 	formattedMsg := fmt.Sprintf(format, args...)
-	e.Msg = err.Error()
-	return errors.Wrap(err, fmt.Sprintf("%s %s", formattedMsg, msg))
+	e.Msg = err.Error() // 可选：记录原始错误内容
+	return fmt.Errorf("%s %s: %w", formattedMsg, msg, err)
 }
 
+// 获取错误码
 func (e Error) GetCode() int {
 	return e.Code
 }
 
+// 获取错误信息
 func (e Error) GetMsg() string {
 	return e.Msg
 }
@@ -58,8 +57,8 @@ func (e Error) ResetMsg(msg string) Error {
 	return e
 }
 
-// AppendMsg 在既有错误信息的基础上追加新的错误信息
-func (e Error) AppendMsg(args ...interface{}) {
+// AppendMsg 在原有错误信息上追加内容
+func (e *Error) AppendMsg(args ...interface{}) {
 	e.Msg += fmt.Sprint(args...)
 }
 
@@ -68,7 +67,7 @@ func (e Error) Is(targetErr error) bool {
 	return errors.Is(targetErr, e)
 }
 
-// As 判断是否为指定错误类型，并将错误信息赋值给目标变量
+// As 判断是否为指定错误类型，并赋值给目标变量
 func (e Error) As(targetErr interface{}) bool {
 	return errors.As(e, targetErr)
 }
